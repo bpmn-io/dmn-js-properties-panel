fa3adc8f226552d6b542b40cc21cea24727f5de3
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2019-06-17 11:18:43 +0200

    feat(factory/EntryFieldDescription): properly escape provided links
    Closes #9

--- test/spec/PropertiesPanelSpec.js
+++ test/spec/PropertiesPanelSpec.js
@@ -210,7 +210,7 @@ describe('PropertiesPanel', function() {
     }));
 
 
-    it('with a link', inject(function(propertiesPanel) {
+    it('with a markdown link', inject(function(propertiesPanel) {
       var descriptionField = getDescriptionField(propertiesPanel._container, '[data-entry=myLinkText]');
 
       expect(descriptionField.textContent).to.be.equal('For details see camunda.org');
@@ -219,6 +219,29 @@ describe('PropertiesPanel', function() {
 
       expect(link.href).to.be.equal('http://www.camunda.org/');
       expect(link.textContent).to.be.equal('camunda.org');
+    }));
+
+
+    it('with an html link', inject(function(propertiesPanel) {
+      var descriptionField = getDescriptionField(propertiesPanel._container, '[data-entry=myHtmlLinkText]');
+
+      expect(descriptionField.textContent).to.be.equal('For details see camunda.org');
+
+      var link = domQuery('a', descriptionField);
+
+      expect(link.href).to.be.equal('http://www.camunda.org/');
+      expect(link.textContent).to.be.equal('camunda.org');
+    }));
+
+
+    it('with a malicious link', inject(function(propertiesPanel) {
+      var descriptionField = getDescriptionField(propertiesPanel._container, '[data-entry=maliciousLinkText]');
+
+      expect(descriptionField.textContent).to.be.equal('For malicious code see [javascript](javascript:alert(1))');
+
+      var link = domQuery('a', descriptionField);
+
+      expect(link).to.not.exist;
     }));
 
   });
