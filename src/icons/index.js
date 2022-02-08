4d4b28d758a7bf602b87f70da66c384150fc7350
Philipp <philippfromme@outlook.com>
2022-02-08 15:11:05 +0100

    chore: code style

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,4 +1,4 @@
-import TextAnnotationIcon from './dmn-icon-text-annotation.svg';
+import AssociationIcon from './dmn-icon-association.svg';
 import AuthorityRequirementIcon from './dmn-icon-authority-requirement.svg';
 import BusinessKnowledgeIcon from './dmn-icon-business-knowledge.svg';
 import DecisionIcon from './dmn-icon-decision.svg';
@@ -7,19 +7,19 @@ import InformationRequirementIcon from './dmn-icon-information-requirement.svg';
 import InputDataIcon from './dmn-icon-input-data.svg';
 import KnowledgeRequirementIcon from './dmn-icon-knowledge-requirement.svg';
 import KnowledgeSourceIcon from './dmn-icon-knowledge-source.svg';
-import AssociationIcon from './dmn-icon-association.svg';
+import TextAnnotationIcon from './dmn-icon-text-annotation.svg';
 
 export default {
-  'TextAnnotation': TextAnnotationIcon,
+  'Association': AssociationIcon,
   'AuthorityRequirement': AuthorityRequirementIcon,
   'BusinessKnowledgeModel': BusinessKnowledgeIcon,
   'Decision': DecisionIcon,
+  'DecisionTable': DRDIcon,
+  'Definitions': DRDIcon,
   'DRD': DRDIcon,
   'InformationRequirement': InformationRequirementIcon,
   'InputData': InputDataIcon,
   'KnowledgeRequirement': KnowledgeRequirementIcon,
   'KnowledgeSource': KnowledgeSourceIcon,
-  'Definitions': DRDIcon,
-  'DecisionTable': DRDIcon,
-  'Association': AssociationIcon
+  'TextAnnotation': TextAnnotationIcon
 };
