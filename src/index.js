e53271bc154b7dd79249a0fb0dffcebdbbe96765
Philipp Fromme <philipp.fromme@camunda.com>
2024-08-27 11:56:49 +0200

    chore: export Zeebe provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export { default as DmnPropertiesPanelModule } from './render';
 export { default as DmnPropertiesProviderModule } from './provider/dmn';
 export { default as CamundaPropertiesProviderModule } from './provider/camunda';
+export { default as ZeebePropertiesProviderModule } from './provider/zeebe';
 
 export { TooltipProvider as ZeebeTooltipProvider } from './contextProvider/zeebe';
 
