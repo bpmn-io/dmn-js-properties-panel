7482ac8b6b5b6a02e1a7879cf52d85f944076424
Philipp Fromme <philipp.fromme@camunda.com>
2024-08-27 12:19:53 +0200

    fix: fix name of Zeebe provider

--- src/provider/zeebe/index.js
+++ src/provider/zeebe/index.js
@@ -1,6 +1,6 @@
 import ZeebePropertiesProvider from './ZeebePropertiesProvider';
 
 export default {
-  __init__: [ 'ZeebePropertiesProvider' ],
-  ZeebePropertiesProvider: [ 'type', ZeebePropertiesProvider ]
+  __init__: [ 'zeebePropertiesProvider' ],
+  zeebePropertiesProvider: [ 'type', ZeebePropertiesProvider ]
 };
