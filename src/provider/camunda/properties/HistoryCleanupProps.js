584c08f28a8cf9bcd86a564aa256901c942e2bbd
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2024-10-14 11:25:49 +0200

    chore: migrate to eslint@9
    Related to https://github.com/bpmn-io/internal-docs/issues/1042

--- src/provider/camunda/properties/HistoryCleanupProps.js
+++ src/provider/camunda/properties/HistoryCleanupProps.js
@@ -55,7 +55,7 @@ function HistoryTimeToLive(props) {
     tooltip: <div>
       <p>
         { translate('Number of days before this resource is being cleaned up. If specified, takes precedence over the engine configuration.') }{' '}
-        <a href="https://docs.camunda.org/manual/latest/user-guide/process-engine/history/" target="_blank" rel="noopener">{ translate('Learn more') }</a>
+        <a href="https://docs.camunda.org/manual/latest/user-guide/process-engine/history/" target="_blank" rel="noopener noreferrer">{ translate('Learn more') }</a>
       </p>
     </div>,
     getValue,
